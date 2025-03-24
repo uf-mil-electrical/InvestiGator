e9a9289e424bf15f1447d5ea50b0b021b8e98c2e
Lester <lester.bonilla@outlook.com>
2025-03-24 16:55:14 -0400

    Adds classes to manage MAVLink connection and methods to subscribe to and publish messages.

---
+++ examples/pubsub.py
@@ -0,0 +1,119 @@
+from queue import Queue, ShutDown
+from threading import Thread, Event
+from pymavlink.dialects.v20 import ardupilotmega as mavlink
+from time import monotonic, sleep
+
+class SubscriptionManager:
+    """
+    Manages and notifies subscribers when a message is published.
+    """
+    def __init__(self, receive_queue: Queue):
+        
+        self.message_subscribers = {}
+        self.receive_queue = receive_queue
+        self.running = Event()
+        self.running.set()
+
+        def reader():
+            while self.running.is_set():
+                try:
+                    message: mavlink.MAVLink_message = receive_queue.get(block=True)
+                    message_type = message.get_type()
+                    if message_type in self.message_subscribers:
+                        self.update_subscribers(message_type, message)
+                except ShutDown:
+                    break
+        
+        self.reader_thread = Thread(target=reader, name="SubManager Thread")
+        self.reader_thread.start()
+
+    def subscribe(self, message_type):
+        """
+        Decorator: Add a function to the mailing list for message_type or attribute. Usage: Decorate with @__class__.__name__.subscribe('message_type') to register a function.
+        """
+        def wrap(function):
+            if message_type not in self.message_subscribers:
+                self.message_subscribers[message_type] = []
+            self.message_subscribers[message_type].append(function)
+        return wrap
+
+    def update_subscribers(self, message_type, message: mavlink.MAVLink_message):
+        """
+        Call functions that have a subscription to message_type.
+        """
+        for function in self.message_subscribers[message_type]:
+            function(message_type, message)
+
+    def close(self):
+        self.receive_queue.shutdown(immediate=True)
+        self.running.clear()
+        self.reader_thread.join()
+
+class PublicationManager:
+    """
+    Manages functions that publish at a given interval.
+    """
+    # TODO: send_queue is not necessary unless publishers are changed to return a message and the thread puts to the q.
+    def __init__(self, send_queue: Queue):
+        
+        self.publishing: dict[str, dict[callable, float, float]] = {}
+        self.send_queue = send_queue
+        self.shortest_period = None
+        self.running = Event()
+        self.running.set()
+
+        def publication_thread():
+            while self.running.is_set() and self.shortest_period is not None:
+                for publisher in self.publishing.values():
+                    if (publisher["last_published"] is None) or (monotonic() - publisher["last_published"]) >= (1/publisher["frequency"]):
+                        publisher["function"]()
+                        publisher["last_published"] = monotonic()
+                sleep(self.shortest_period)
+
+        self.publish_thread = Thread(target=publication_thread, name="Publication Thread", daemon=True)
+        self.publish_thread.start()
+
+    def publish(self, message_name, frequency):
+        """
+        Decorator: Registers a function to be published at given frequency.
+        """
+        if frequency > 50:
+            print("Frequency too large. Please choose a frequency less than or equal to 50Hz.")
+            return
+
+        if  self.shortest_period is None or 1/frequency < self.shortest_period:
+            self.shortest_period = 1 / frequency
+
+        def wrap(function):
+            if message_name in self.publishing:
+                print("This function is already registered.")
+            else:
+                self.publishing[message_name] = {"function": function, "frequency": frequency, "last_published": None}
+            return function
+        return wrap
+    
+    def register_publisher(self, message_name, frequency, function):
+        """
+        Registers a function to be published at given frequency.
+        """
+        if frequency > 50:
+            print("Frequency too large. Please choose a frequency less than or equal to 50.")
+            return
+
+        if message_name in self.publishing:
+            print("This function is already registered.")
+        else:
+            self.publishing[message_name] = {"function": function, "frequency": frequency, "last_published": None}
+
+    def remove_publisher(self, message_name):
+        """
+        Removes a function from publishing list.
+        """
+        if message_name in self.publishing:
+            del self.publishing[message_name]
+        else:
+            print("Function not a publisher.")
+
+    def close(self):
+        self.running.clear()
+        self.publish_thread.join()
