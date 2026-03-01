e15c9ec19b5fff18bd7c2267ec85e76d742b1028
Lester <lester.bonilla@outlook.com>
2026-03-01 14:51:46 -0800

    Update main.py to expect command_long messages instead of named_value_int messages

--- src/main.py
+++ src/main.py
@@ -4,25 +4,26 @@ from pymavlink.dialects.v20 import ardupilotmega as mavlink
 from constants import Radio, Robot
 from dataclasses import dataclass
 import time
+from pymavlink import mavutil
 
 def test(vehicle: VehicleManager):
     
-    vehicle.camera.switch_mode("UAV Recovery")
-    vehicle.set_mode(target_mode="GUIDED")
+    #vehicle.camera.switch_mode("UAV Recovery")
+    vehicle.set_mode(target_mode = "GUIDED")
     vehicle.wait_for_armed()
     vehicle.takeoff(alt_m=10)
 
     vehicle.move_body_frd_position(forward_m=5, right_m=4, down_m=-10, timeout_s=20)
     print("Positioned for search.")
-    detection_gps = vehicle.search_for_detection("UAV Recovery")
-    print("Search Complete")
-    if detection_gps is not None:
-        vehicle.center_on_marker(timeout_s=100, target_distance_m=0.25)
+    #detection_gps = vehicle.search_for_detection("UAV Recovery")
+    #print("Search Complete")
+    #if detection_gps is not None:
+        #vehicle.center_on_marker(timeout_s=100, target_distance_m=0.25)
 
-    vehicle.set_mode("LAND")
+    vehicle.set_mode(target_mode = "LAND")
     print("DONE")
 
-    vehicle.close()
+    #vehicle.close()
 
 @dataclass
 class MissionState:
@@ -31,23 +32,26 @@ class MissionState:
 if __name__ == "__main__":
 
     # Make connection
-    connection = MAVConnection(Radio.SIMULATION, source_system=Robot.INVESTIGATOR)
+    connection = MAVConnection(Radio.DRONE_SIM, source_system=1, source_component=mavlink.MAV_COMP_ID_ONBOARD_COMPUTER )
     vehicle = VehicleManager(mav_connection=connection)
 
     # Instantiate mission state to detect when a mission message is received
     mission_state = MissionState()
 
-    @vehicle.subscribe(mavlink.MAVLINK_MSG_ID_NAMED_VALUE_INT)
-    def handle_named_value_int(message):
-        if message.name.decode('utf-8') == "INVGATOR":
-            print(f"STATUSTEXT: {message.text}")
-            if message.value == 1:
+    @vehicle.subscribe(mavlink.MAVLink_command_long_message.msgname)
+    def handle_command_int(message):
+        if message.command == mavlink.MAV_CMD_USER_1:
+            print("Mission Selection Message Received.")
+            if message.param1 == 1:
                 mission_state.mission_selection = 1
-            elif message.value == 2:
+            elif message.param1 == 2:
                 mission_state.mission_selection = 2
 
-    print("Listening for mission selection.")
     while True:
+        if mission_state.mission_selection == 0:
+            print("Listening for mission selection.")
+            mission_state.mission_selection = -1
+
         if mission_state.mission_selection == 1:
             print("Starting Mission 1")
             test(vehicle)
