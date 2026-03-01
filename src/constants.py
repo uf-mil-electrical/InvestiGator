b1876a1167e830452608ab129e286b561b567b0e
Lester <lester.bonilla@outlook.com>
2026-03-01 14:50:16 -0800

    Redefine constant connection strings for simulation

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,6 @@
-SIMULATION_RADIO = 'udpin:0.0.0.0:14550' 
-GROUND_CONTROL_RADIO_LINUX = "/dev/serial/by-id/usb-FTDI_TTL232R-3V3_FTDCKG37-if00-port0"
-ROVER_RADIO_LINUX = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_BG00HS6A-if00-port0"
-
 class Radio():
-    SIMULATION = 'udpin:0.0.0.0:14550' 
+    GROUND_CONTROL_SIM = 'udpin:127.0.0.1:14551' 
+    DRONE_SIM = 'udpin:127.0.0.1:14552'
     GROUND_CONTROL_LINUX = "/dev/serial/by-id/usb-FTDI_TTL232R-3V3_FTDCKG37-if00-port0"
     ROVER_LINUX = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_BG00HS6A-if00-port0"
 
