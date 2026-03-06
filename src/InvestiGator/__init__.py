7f4992ef7255d93ded08dcd9b9ad13d56958c442
Lester <lester.bonilla@outlook.com>
2026-03-05 20:04:45 -0800

    Simplify __init__ for InvestiGator package

--- src/InvestiGator/__init__.py
+++ src/InvestiGator/__init__.py
@@ -1,10 +1,4 @@
-__all__ = ["camera", "constants", "mavconnection", "vehicle_properties", "vehicle"]
-
-from . import camera
-from . import constants
-from . import vehicle
-from . import vehicle_properties
-
 from .mavconnection import MAVConnection
 from .camera import Camera
 from .vehicle import VehicleManager
+from .vehicle_properties import Status, Location, MavFrameGlobal, MavFrameLocalNed, MavFrameGlobalRel, MavFrameLocalENU, MavFrameLocalOffsetNED, MavFrameBodyFRD, MavFrameLocalFRD, MavFrameLocalFLU, Attitude
