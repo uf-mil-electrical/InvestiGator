c638c4bac472f217d137b70747bd84c0b54dcf21
Lester <lester.bonilla@outlook.com>
2026-03-06 04:31:41 -0800

    Remove constants.py in favor of config.toml

--- src/InvestiGator/__init__.py
+++ src/InvestiGator/__init__.py
@@ -2,4 +2,3 @@ from .mavconnection import MAVConnection
 from .camera import Camera
 from .vehicle import VehicleManager
 from .vehicle_properties import Status, Location, MavFrameGlobal, MavFrameLocalNed, MavFrameGlobalRel, MavFrameLocalENU, MavFrameLocalOffsetNED, MavFrameBodyFRD, MavFrameLocalFRD, MavFrameLocalFLU, Attitude
-from .constants import Radio, Robot
