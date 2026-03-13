090a7443ef442e30db19eb0c3710a41100a2f9d5
Lester <lester.bonilla@outlook.com>
2026-03-13 01:27:48 -0700

    Clear out compose to start fresh. Set default theme

--- src/gc_ui/textual_ui.py
+++ src/gc_ui/textual_ui.py
@@ -6,43 +6,21 @@ MISSIONS = [("Mission 1", 0), ("Mission 2", 1), ("Mission 3", 2)]
 
 class MissionControl(App):
     """
-    Practice Textual app to test UI.
+    Mission Control textual app
     """
 
-    CSS = """
-    .with-border {
-        border: heavy green;
-    }
-    """
+    CSS_PATH = "gc.tcss"
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Header()
 
-        with Horizontal():
-            with Vertical(id="left"):
-                with Vertical(id="status", classes="with-border"):
-                    yield Label("--- Drone Status ---")
-                    yield Label("Mode: --", id="mode")
-                    yield Label("Armed: --", id="armed")
-                    yield Label("Relative Altitude: --", id="altitude")
-                    yield Label("GPS: --", id="gps")
-                    yield Label("NED Position: --", id="ned")
-                    yield Label("State: --", id="state")
-
-                with Vertical(id="mission_select", classes="with-border"):
-                    yield Label("--- Mission Select ---")
-                    yield Select(options=MISSIONS, id="mission_select")
-                    yield Button("Start Mission", id="start_mission", variant="success")
-
-            with Vertical(id="mission_log_display", classes="with-border"):
-                yield Label("--- Mission Log ---")
-                yield RichLog(id="mission_log")
+        
                 
         yield Footer()
 
     def on_mount(self):
-        self.query_one("#mission_log", RichLog).write("Testing")
+        self.theme = "nord"
 
 if __name__ == "__main__":
     app = MissionControl()
