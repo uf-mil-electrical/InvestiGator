1f0d4d9720cdfdfc88c86ee40a7bec26155b1a27
Lester <lester.bonilla@outlook.com>
2026-03-13 16:10:58 -0700

    Add mission abort css class and system command buttons. Add button press behavior for system command funcitons

--- src/gc_ui/textual_ui.py
+++ src/gc_ui/textual_ui.py
@@ -1,5 +1,5 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, Button, Select, Label, RichLog
+from textual.widgets import Header, Footer, Button, Select, Label, RichLog, Static
 from textual.containers import Horizontal, Vertical, Center
 
 MISSIONS = [("Mission 1", 0), ("Mission 2", 1), ("Mission 3", 2)]
@@ -29,13 +29,15 @@ class MissionControl(App):
                             yield Button("Start Mission", variant="primary", id="start_mission_button")
                         with Center():
                             yield Button("Cancel Mission", variant="error", id="cancel_mission_button")
+                        with Center():
+                            yield Static(id="uncontrolled_state_message", content="Drone in Uncontrolled State. \nSet mode to GUIDED to clear.")
 
                     with Vertical(id="system_panel", classes="panel") as system_panel:
                         system_panel.border_title = "System Commands"
                         with Center():
                             yield Button(id="recording_button", label="Toggle Recording")
                         with Center():
-                            yield Button(id="mission_abort_button", label="Abort")
+                            yield Button(id="abort_mission_button", label="Abort")
                         with Center():
                             yield Button(id="set_guided_button", label="Enable Guided")
 
@@ -58,6 +60,22 @@ class MissionControl(App):
         elif event.button.id == "cancel_mission_button":
             self.remove_class("mission_started")
             self.query_one(Select).disabled = False
+
+        elif event.button.id == "abort_mission_button":
+            if not self.has_class("mission_abort"):
+                self.add_class("mission_abort")
+                self.remove_class("mission_started")
+
+        elif event.button.id == "set_guided_button":
+            if not self.has_class("guided_mode"):
+                self.add_class("guided_mode")
+            if self.has_class("mission_abort"):
+                self.remove_class("mission_abort")
+                self.query_one(Select).disabled = False
+
+        elif event.button.id == "recording_button":
+            if not self.has_class("recording_enabled"):
+                self.add_class("recording_enabled")
 
 
     def on_mount(self):
