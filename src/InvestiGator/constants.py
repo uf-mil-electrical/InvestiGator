6a15cfed7560100e48bb03e40a8cbeb70e628605
Lester <lester.bonilla@outlook.com>
2026-03-15 16:57:26 -0700

    Remove deprecated command values, add MIL_STATE heartbeat status values

--- src/InvestiGator/constants.py
+++ src/InvestiGator/constants.py
@@ -1,6 +1,11 @@
 from pymavlink.dialects.v20 import ardupilotmega as mavlink
 
+# Commands for COMMAND_LONG message
 MIL_MISSION_CMD = mavlink.MAV_CMD_USER_1
-MIL_MISSION_CANCEL = mavlink.MAV_CMD_USER_2
-MIL_MISSION_ABORT = mavlink.MAV_CMD_USER_3
 MIL_SYSTEM_CMD = mavlink.MAV_CMD_USER_4
+
+# System Status for heartbeat (uint8_t)
+MIL_STATE_CONNECTING = 9
+MIL_STATE_STANDBY = 10
+MIL_STATE_OVERRIDE = 11
+MIL_STATE_MISSION = 12
